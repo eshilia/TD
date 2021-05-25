6b5c772eaf3b52c7c620db718c8e5883ac625f86
JongHOon <corea_89@naver.com>
2021-05-25 21:50:40 +0900

    1

---
+++ Assets/Scritps/SceneMaker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Index
+{
+    public int x;
+    public int y;
+    public Index(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+[CreateAssetMenu(menuName = "TileMaker")]
+public class SceneMaker : ScriptableObject
+{
+
+    [SerializeField]
+    protected List<Index> _Tile = new List<Index>();    
+    public List<Index> Tile { get { return _Tile; } }
+
+}
